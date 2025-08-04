e95b2654b08eab39edad44f4def9fb927621f4ef
Sarah Irene Riya <sarahireneriya@gmail.com>
2025-08-04 23:14:41 +0530

    Add files via upload

---
+++ e0223026 5.R
@@ -0,0 +1,41 @@
+#Arithmetic Operators
+a<-10
+b<-3
+c<-a+b
+c
+
+v1<-2.5
+v2<-4
+r<-v1/v2
+r
+
+a<-sqrt(v2)
+a
+
+g<-"hello"
+n<-"bob"
+m<-paste(g,n)
+
+#Logical operators
+# ==
+# !=
+# <
+# >
+# <=
+# >=
+# !
+# |
+# &
+# isTRUE()
+
+result <- 4<5
+result
+typeof(result)
+
+result2 <- !(5>1)
+result2
+
+result | result2
+result & result2
+
+isTRUE(result)
